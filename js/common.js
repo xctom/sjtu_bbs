bd8499661e095470d933dfe939db74d2abfd6eb2
xctom <xctom@sjtu.edu.cn>
2014-02-09 15:43:26 +0800

    1、完成右边动态菜单栏 2、完成login界面素材收集

--- js/common.js
+++ js/common.js
@@ -9,4 +9,38 @@ $(document).ready(function(){
     // 初始化
     headroom.init(); 
     
+    /**
+     *改写右边菜单效果
+     **/
+    (function(){
+        var menu = $("ul.dropdown-menu");
+        menu.css("margin-right","-" + $(this).width() + "px");
+        menu.width(300);
+        menu.height(window.outerWidth - $("header").height());
+    })();
+
+
+    /*设置移动端和pc端的不同表现*/
+    $("a#right-menu").click(function(){
+        
+        if(document.width > 768){
+            
+            var menu = $("ul.dropdown-menu");
+            var menu_width = menu.width() + "px";
+
+            if(menu.css("opacity") == 0){
+                menu.css("margin-right","0px");
+            }else{
+                menu.css("margin-right","-" + menu_width);
+            }
+
+            menu.toggleClass("menu-in");
+
+        }else{
+
+            $("#fat-menu").toggleClass("open");
+        }  
+    });
+
+
 });
