f18c507a7f504ff853757d8ea05d34c9d5debf6a
xctom <xctom@sjtu.edu.cn>
2014-02-21 12:41:14 +0800

    简约版讨论区，待改进

--- js/common.js
+++ js/common.js
@@ -8,39 +8,5 @@ $(document).ready(function(){
     var headroom  = new Headroom(myElement);
     // 初始化
     headroom.init(); 
-    
-    /**
-     *改写右边菜单效果
-     **/
-    // (function(){
-    //     var menu = $("ul.dropdown-menu");
-    //     menu.css("margin-right","-" + $(this).width() + "px");
-    //     menu.width(300);
-    //     menu.height(window.outerWidth - $("header").height());
-    // })();
-
-
-    // /*设置移动端和pc端的不同表现*/
-    // $("a#right-menu").click(function(){
-        
-    //     if(document.width > 768){
-            
-    //         var menu = $("ul.dropdown-menu");
-    //         var menu_width = menu.width() + "px";
-
-    //         if(menu.css("opacity") == 0){
-    //             menu.css("margin-right","0px");
-    //         }else{
-    //             menu.css("margin-right","-" + menu_width);
-    //         }
-
-    //         menu.toggleClass("menu-in");
-
-    //     }else{
-
-    //         $("#fat-menu").toggleClass("open");
-    //     }  
-    // });
-
 
 });
