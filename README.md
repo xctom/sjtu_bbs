0c1d960671b21e785030f471a7810f59704276e4
ImaginationZ <frequencyhzs@gmail.com>
2014-03-16 19:48:41 +0800

    All-new design; almost finish sidebar in PC mode

--- README.md
+++
@@ -1,9 +0,0 @@
-sjtu_bbs
-========
-
-新版水源皮肤
-
-* Stylesheet 使用 bootstrap 3
-* 图标使用 font-awesome 4.0
-* UI框架使用flat-UI
-* 富文本编辑器使用tinymace
---
+++ js/main.js
@@ -0,0 +1,16 @@
+//For sidebar
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+});
+
+//For Tooltips
+$(function(){
+	var options={
+		animation: true,
+		trigger: 'hover',
+		placement: 'right',
+		container: '.sidebar-tooltip'
+	}
+	$(".sidebar-nav li a").tooltip(options);
+});
